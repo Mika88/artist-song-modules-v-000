aba60ede162c275c2f4d5c6030f02e13f5d5dfe9
Mika88 <mikaspivak@gmail.com>
2018-08-11 18:32:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,18 +19,18 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  #def self.reset_all
+  #  self.all.clear
+  #end
 
-  def self.count
-    @@artists.count
-  end
+  #def self.count
+  #  @@artists.count
+  #end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
+  #def add_song(song)
+  #  @songs << song
+  #  song.artist = self
+  #end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
